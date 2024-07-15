9dda205e38d219a3cf52acd73e24085ec01434a1
malikobaid81 <122255795+malikobaid81@users.noreply.github.com>
2024-07-15 20:24:30 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "security_group" {
     default     = "terra-jenkins-SG" 
 } 
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "terra-jenkins-key" 
-}
-
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
